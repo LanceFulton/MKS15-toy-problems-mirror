3af0ebf148c1d54988138136f536be9925cfe0a0
TravisJN <tdizzletrav@gmail.com>
2015-03-10 09:57:51 -0700

    almost finished RPS

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,51 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
   // TODO: your solution here
+  //create a loop that loops through 1 2 3
+  //at slot 1, loop through three possibilities
+  //at slot 2, loop through three possibilities, etc...
+  var results = [];
+  var plays = ["rock", "paper", "scissors"];
+  var round = [];
+  var roundCount = 0;
+
+  // for (var j = 0; j < 3; j++){
+  //   //call cycleAllPlays at each index
+  //   //at index 0, push rock, index 1 push rock, index 2 - cycle all plays
+  //   round[j] = cycleAllPlays();
+  // }
+
+  var cycleAllPlays = function(i, j, l) {
+    round.push(plays[i]);
+    round.push(plays[j]); 
+    round.push(plays[l]);   
+    results.push(round);
+    round = [];
+  }
+  
+
+var i = 0,
+    j = 0,
+    l = 0;
+  //for index 0 1 2 only change index 2, for index 3 4 5 only change index 1, for index 6 7 8 only change index 0
+
+  //at array[0], push new array [rock, rock, rock], then push new array [rock, rock, paper], etc.
+  for (i; i < 3; i++){
+    //round[roundCount] = [];
+    cycleAllPlays(i, j, l);
+      for (j; j < 3; j++) {
+        cycleAllPlays(i, j, l);
+        for (l; l < 3; l++) {
+          cycleAllPlays(i, j, l);
+        }     
+    }
+  }
+    //results.push(round);
+    
+  // }
+  console.log(results);
+
 };
+rockPaperScissors();
