d5e8ce28a57c42035cfb4976563083522d5fd598
LanceFulton <lsfaccounts@gmail.com>
2015-03-10 09:58:33 -0700

    incomplete, but close

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,50 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+	// create an array of possible inputs for each stage of the sequence
+	var possibles = ['rock', 'paper', 'scissors'];
+
+	// and a variable to keep track of which answer should currently be taken from the array, for each stage
+	var firstCurrent = 0;
+	var secondCurrent = 0;
+	var thirdCurrent = 0;
+
+	// and a variable to count how many times the second and third arrays have been run through
+	var secondCount = 0;
+	var thirdCount = 0;
+
+	// and result arrays
+	var currentResult = [];
+	var finalResult = [];
+
+  	// outer loop
+  	while(thirdCount <= 27){
+  		currentResult.push(possibles[firstCurrent]);
+  		currentResult.push(possibles[secondCurrent]);
+  		currentResult.push(possibles[thirdCurrent]);
+  		thirdCount++
+  		thirdCurrent++
+  		console.log("currentResult: " + currentResult);
+
+  		//after third stage has pushed everything once, restart third stage and increment second stage
+  		if (thirdCount % 3 === 0){
+  			finalResult.push(currentResult);
+  			currentResult = [];
+  			secondCount++;
+  			secondCurrent++;
+  			thirdCurrent = 0;
+  		}
+
+  		//after second stage has pushed everything once, restart second stage and increment third stage
+  		if (secondCount % 3 === 0){
+  			firstCurrent++;
+  			secondCurrent = 0;
+  		}
+  	}
+  	
+  	return finalResult;
+
 };
+
+
