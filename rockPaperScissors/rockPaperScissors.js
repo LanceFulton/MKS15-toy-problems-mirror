0b181c8a06459fe1c5836c8042d64a952d7b9ba9
sperrow <mattjsperry@gmail.com>
2015-03-10 10:00:32 -0700

    Refactor to for-in loops

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,19 +17,39 @@
 *
 */
 
-var rockPaperScissors = function (rounds) {
-  // TODO: your solution here
-  var moves = ['rock', 'paper', 'scissors']
+var rockPaperScissors = function () {
+  var moves = {
+    0: 'rock',
+    1: 'paper',
+    2: 'scissors'
+  };
   var sequences = [];
 
-  for(var i = 0; i < 3; i++) {
-    for(var j = 0; j < 3; j++) {
-      for(var k = 0; k < 3; k++) {
-        var seq = [moves[i], moves[j], moves[k]];
+  for(var k1 in moves) {
+    for(var k2 in moves) {
+      for(var k3 in moves) {
+        var seq = [moves[k1], moves[k2], moves[k3]];
         sequences.push(seq);
       }
     }
   }
+
+  // partial attempt at extra credit
+  // var seq = [];
+
+  // var recurse = function(rounds) {
+  //   if(rounds > 0) {
+  //     for(var k in moves) {
+  //       seq.push(moves[k]);
+  //       recurse(rounds - 1)
+  //     }
+  //   } else {
+  //     sequences.push(seq);
+  //     seq = [];
+  //   }
+  // }
+
+  // recurse(rounds);
 
   return JSON.stringify(sequences);
 };
