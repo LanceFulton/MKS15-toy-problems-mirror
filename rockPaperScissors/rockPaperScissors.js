f67f1888998806d2607f22a92188f6bf72432543
Sean Conroy <conroysean@gmail.com>
2015-03-10 09:59:27 -0700

    mostly complete

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -16,8 +16,17 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
+var elements = [["rock", "paper", "scissors"], ["rock", "paper", "scissors"], ["rock", "paper", "scissors"]];
+var rockPaperScissors = function (arr) {
+  var possibleThrows = [];
+  var remainingThrows = rockPaperScissors(arr.slice(1));
+    for (var i = 0; i < remainingThrows; i++){ 
+      for (var j = 0; j < arr[0].length -1; j++) { 
+      	possibleThrows.push(arr[0][j] + remainingThrows[i]); 
+      }
+    }
+  return possibleThrows;
+}
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+console.log(rockPaperScissors(elements));
+
