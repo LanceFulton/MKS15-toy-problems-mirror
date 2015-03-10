65aabf9dc36d83b92b502a07f8134f4d718c4471
sperrow <mattjsperry@gmail.com>
2015-03-10 09:28:42 -0700

    Basic solution for rockPaperScissors.js

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,19 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function (rounds) {
   // TODO: your solution here
+  var moves = ['rock', 'paper', 'scissors']
+  var sequences = [];
+
+  for(var i = 0; i < 3; i++) {
+    for(var j = 0; j < 3; j++) {
+      for(var k = 0; k < 3; k++) {
+        var seq = [moves[i], moves[j], moves[k]];
+        sequences.push(seq);
+      }
+    }
+  }
+
+  return JSON.stringify(sequences);
 };
