a97d17033c7f5b6ed79fc9f413a3e7a40732a734
Joseph Pei <pei_joseph@yahoo.com>
2015-03-11 09:44:13 -0700

    Created a working nonrepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,20 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var remaining = string.slice();
+  while (remaining.length !== 0) {
+    var isRepeat = false;
+    for (var i=remaining.length-1; i>0; i--) {
+      if (remaining[i] === remaining[0]) {
+        isRepeat = true;
+        remaining = remaining.slice(0,i) + remaining.slice(i+1);
+      }
+    }
+    if (!isRepeat) {
+      return remaining[0];
+    } else {
+      remaining = remaining.slice(1);
+    }
+  }
+  return '';
 };
