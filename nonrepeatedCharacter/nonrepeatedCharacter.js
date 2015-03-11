8e5009e7eb3be49695c6f74018cdeab89a76ff94
Trace Thompson <tracethmpsn@gmail.com>
2015-03-11 09:47:17 -0700

    Solved non repeated characters

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,22 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var repeatCount = {};
+
+  for(var i = 0;i < string.length; i++){
+    var current = string[i];
+    if(repeatCount[current]){
+      repeatCount[current] = -1;
+    }
+    else {
+      repeatCount[current] = 1
+    }
+  }
+  for(j = 0; j < string.length; j++){
+    var currentLetter = string.charAt(j);
+    if(repeatCount[currentLetter] === 1){
+      return currentLetter;
+    } 
+  }
+  return "all letters repeated";
 };
