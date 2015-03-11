aa4d03f4ae012929861b5bcf771daa51f57273d2
Thomas Logan <tdloganj@gmail.com>
2015-03-11 09:57:56 -0700

    ALMOST completed firstNonRepeatedCharacter. It almost works properly, it just isn't taking the 0 index into account.

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -1,11 +1,11 @@
-/**
- * Given an arbitrary input string, return the first nonrepeated character in
- * the string. For example:
- *
- *   firstNonRepeatedCharacter('ABA'); // => 'B'
- *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
- */
-
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+
+  var letterArray = string.split(''); //String is split into array of characters
+  var previous; //Setting variable to represent our array that will be sliced below
+  for (var i = 0; i < letterArray.length; i++){
+    previous = letterArray.slice(0, letterArray[i]); //This sets our previous array of letters to a slice of our letter array
+    if (previous.indexOf(letterArray[i], 0) === -1){ //Using indexOf, if our letter is not inside of our sliced array, we know that it is an original character
+      return letterArray[i]; //Return original
+    }
+  }
 };
