3a14f53c4262ab18a4d2b18503b24bdd16076cc1
Bryan Lee <bclee@ucdavis.edu>
2015-03-11 09:46:28 -0700

    added solution for firstNonRepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,21 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var letterCounts = {};
+  var chars = [];
+  for (var i = 0; i < string.length; i++){
+    if (letterCounts[string[i]])
+      letterCounts[string[i]] += 1;
+    else{
+      letterCounts[string[i]] = 1;
+      chars.push(string[i]);
+    }
+  }
+  
+  for (var i = 0; i < chars.length; i++){
+    if (letterCounts[chars[i]] === 1)
+      return chars[i];
+  }
+  
+  return null;
 };
