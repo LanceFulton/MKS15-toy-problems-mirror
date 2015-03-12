f066b04f130e2542ae51d0b60a79f970ee7b73d6
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-03-12 08:40:29 -0700

    3/11 Toy Problem Mostly Complete

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,23 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  var storage = [];
+  var index = 1;
+  var objectStorage = {};
+  for (var i = 0; i < string.length; i++) {
+    storage.push(string[i]);
+  }
+  for (var j = 0; j < storage.length; j++) {
+    if ( objectStorage[storage[j]] ) {
+      objectStorage[storage[j]] += 1 ;
+    } else {
+      objectStorage[storage[j]] = 1;
+    }
+  };
+  for (var key in objectStorage) {
+    if (objectStorage[key] === 1) {
+      //return objectStorage.key;
+    }
+  }
 };
+//Ran out of time. Just need it to return the key when the value is equal to 1. 
