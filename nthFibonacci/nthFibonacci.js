9332e96eb6fda9eaaaf55290a9de0bd7bf420ccc
Seven Li <sevenlist0110@gmail.com>
2015-03-13 09:48:50 -0700

    Finished nthFibonacci using recursion

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -22,7 +22,33 @@
 
 var nthFibonacci = function (n) {
   // TODO: implement me!
+  /* solution 1 */
+  // base case, if n is 0, or 1, just return 0 or 1 (since there's no previous 2 element for n=0 and n=1)
+  if (n===0 || n ===1) {
+    return n;
+  }
+  // else, recursively call to return fib(n-1)+fib(n-2)
+  else {
+    return nthFibonacci(n-1) + nthFibonacci(n-2);
+  }
+
+
+  /* solution 2
+  // algorithm
+  // construct an array of fib numbers, fill index 0 with 0, index 1 with 1. (default initial)
+  // for i= 2..n
+  // fib_array[i]= fib_array[i-1]+fib_array[i-2];
+  // return fib_array[n]
+  // this method may be good if want to repeatedly call the fibonacci function to generate number.... 
+  */
 };
 
+/*
+console.log(nthFibonacci(1)); // 0
+console.log(nthFibonacci(2)); // 1
+console.log(nthFibonacci(3)); // 1
+console.log(nthFibonacci(4)); // 2
+console.log(nthFibonacci(10)); // 55
+*/
 
 
