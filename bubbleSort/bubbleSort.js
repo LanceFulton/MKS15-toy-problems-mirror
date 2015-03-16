bbb948b839f7f461cd4691afd350b86a82dad631
Seven Li <sevenlist0110@gmail.com>
2015-03-15 21:17:24 -0700

    Finished bubbleSort, including answering the questions and implement extra credit

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -14,14 +14,24 @@
  *
  * QUERY: What's the time complexity of your algorithm? If you don't already
  * know, try to intuit this without consulting the Googles.
+    ANSWER: the time complexity of bubble sort is O(n^2) worst case, which means
+    each iteration it examine with O(n), and there's total of n iterations, 
+    so O(n*n) worst case.
+    avg case: O(n^2)
+    best case: O(n) because still need to iterate through loop once
  *
  * Extra credit: Optimization time! During any given pass, if no elements are
  * swapped we can assume the list is sorted and can exit the function early.
  * After this optimization, what is the time complexity of your algorithm?
+    ANSWER: it's still O(n^2) worst case. 
+    avg case: this can be cut into half the time on avg, but O(1/2n^2) is still O(n^2)
+    best case: O(n)
  *
  * Moar credits: Do you need to consider every element every time you iterate
  * through the array? Make it happen, boss. Again: Has the time complexity of
  * your algorithm changed?
+   ANSWER: Nop, still O(n^2) worst case: n times to iterate, and each iteration checks i of element. 
+   so O(n*i) =O(n*1/2n)
 */
 
 /*
@@ -38,4 +48,40 @@ var i;
 
 var bubbleSort = function(array) {
   // Your code here.
+  // assign the index of sorted array the index of last element
+  var sortedIndex=array.length-1;
+  // as long as the index of sorted array is >0, we continue looping through array.
+  var swapped=false;
+  while (sortedIndex>0){
+  // as we iterate through the array once, swap the larger element to sink downward.
+    swapped=false; // reset the indicator indicating swapping took place
+    for ( var i=0; i<sortedIndex; i++){
+      var temp;
+      // if the current element is greater than next, perform swapping to sink the larger element down
+      if (array[i]>array[i+1]){
+        temp=array[i];
+        array[i]=array[i+1];
+        array[i+1]=temp;
+        swapped=true;
+      }
+      // else, do nothing
+    }
+  // as we finished each iteration, decrement index of sorted array by 1,
+    sortedIndex--;
+    // if finished iterating the current array and find no swapping taken place, 
+    // then sorting is finished, exiting while loop
+    if (!swapped){
+      break;
+    }
+  // else, if swapping took place
+  // and repeat the sorting process
+  }
+  return array;
 };
+
+
+/*
+console.log(bubbleSort([2,1,3]));
+console.log(bubbleSort([4,3,2,1]));
+console.log(bubbleSort([5,3,3, 7, 1, 9]));
+*/
