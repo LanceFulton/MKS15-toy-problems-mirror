4cf3a0aec0eb525ad253aeda5c52122c2d6d6dcf
Thomas Logan <tdloganj@gmail.com>
2015-03-17 09:56:36 -0700

    commonCharacters complete. Ignores white space and duplicate characters!

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,7 +10,43 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+//How can i approach this?
+
+
+function contains(index, array){//------------------------Contains function is being used to check our common letters to make sure we dont duplicate them
+  var count = 0;
+  for (var i = 0; i < array.length; i++){
+    if (index === array[i]){
+      count++;
+      break;
+    }
+  }
+  if (count > 0){
+    return true;
+  }
+}
+
+
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  //naive implementation
+  var common = [];//---------------------------------------Empty array for our common characters
+  var letters1 = string1.split('');//----------------------splitting both inputs into letter arrays
+  var letters2 = string2.split('');
+  for(var i = 0; i < string1.length; i++){
+
+    if (letters1[i] !== " "){//----------------------------handles whitespaces.
+      if (!contains(letters1[i], common)){
+        for (var j = 0; j < string2.length; j++){
+          if (letters1[i] === letters2[j]) {
+            common.push(letters1[i]);
+            break;//-----------------------------------------If we find a common character, we want to break the current loop to avoid unnecessary duplications.
+          }
+        }
+      }
+    }
+  }
+  return common;
 };
+
