7bfbb90c90d7d1bf1c1e5ee972742364dc64bd37
MKS-Student <mks.student@hackreactor.com>
2015-03-17 09:59:39 -0700

    Completed commonCharacters + extra credit

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -11,6 +11,36 @@
  */
 
 
-var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+var commonCharacters = function(string1) {
+  var result = [];
+  var allChars = {};
+
+  //loop through each argument
+  for(var j = 1; j < arguments.length; j++){
+  	var uniqChars = {};
+  	var string = arguments[j];
+  	//loop through the string and check for unique characters
+	  for(var i=0; i<string.length; i++){
+	  	var currChar = string[i];
+	  	if(uniqChars[currChar] === undefined){
+	      uniqChars[currChar] = true;
+	  	}else if(uniqChars[currChar] === true || currChar !== " "){
+	      uniqChars[currChar] = false;
+	  	}
+	  }
+	  //copy unique characters into all Characters object
+	  for(var key in uniqChars){
+	  	allChars[key] = uniqChars[key];
+	  }
+  }
+
+  //check characters from string against allChars object
+  for(var i=0; i<string1.length; i++){
+  	var currChar = string1[i];
+  	if(allChars[currChar] === true){
+      result.push(currChar);
+  	}
+  }
+
+  return result.join("");
 };
