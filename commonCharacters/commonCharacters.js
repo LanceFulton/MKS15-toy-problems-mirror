dfc175e8177d0c8f68263eaac9d0f2d556367bf6
Seven Li <sevenlist0110@gmail.com>
2015-03-17 09:56:01 -0700

    Finished "commonCharacters" toy problem with extra credit

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,7 +10,38 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
-
+// finished implementation with extra credit
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  var map={};
+  for (var i=0; i<arguments.length; i++){
+    // get rid of white space and then arraylize
+    var str_arr=arguments[i].split(' ').join('').split('');
+    if (i === 0) { // first time
+      for (var j=0; j<arguments[0].length; j++) {
+        if (!map[str_arr[j]]) { // initialize the map
+        map[str_arr[j]]=true;
+        }
+      }
+    }
+    else { // if not fist time
+      for (var key in map){
+        if (str_arr.indexOf(key) === -1) { // if not unique
+          // remove it from map
+          delete map[key];
+        }
+      }
+    }  
+  }
+  var common_arr=[];
+  for (var key in map){
+    common_arr.push(key);
+  }
+  return common_arr.sort().join('');
 };
+
+/*
+console.log(commonCharacters('abc', 'aef', 'aasa'));
+console.log(commonCharacters('acexivou', 'aegihobu'));
+console.log(commonCharacters('a cexivou', 'a egihobu'));
+*/
