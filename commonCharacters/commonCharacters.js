4b564c3397c7813dabba58ee03832965db24d498
travisjn <tdizzletrav@gmail.com>
2015-03-17 09:56:46 -0700

    Complete common characters function

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,7 +10,52 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+var getUniqueChars = function(string, array){
+
+  for (var i = 0; i < string.length; i++){
+    if (checkChar(string[i], i, string) === true){
+      array.push(string[i]);
+    }
+  }
+
+  return array;
+}
+
+var getDuplicateChars = function(string, obj){
+
+  for (var i = 0; i < string.length; i++){
+    if (checkChar(string[i], i, string) === false){
+      obj[string[i]] = 1;
+    }
+  }
+
+  return obj;
+}
+
+var checkChar = function(character, index, string){
+  for (var i = 0; i < string.length; i++){
+    if (index !== i && character === string[i]){
+      return false;
+    }
+  }
+  return true;
+}
 
 var commonCharacters = function(string1, string2) {
   // TODO: Your code here!
+  var uniqueChars = [];
+  var result = [];
+  var sameCharObj = {};  //object to store the duplicate characters, dupes will be stored as keys to prevent storing the same letter twice
+
+  //get unique characters of every string passed into function
+  for (var i = 0; i < arguments.length; i++){
+    getUniqueChars(arguments[i], uniqueChars);
+  }
+
+  //store all duplicates in an object as keys
+  sameCharObj = getDuplicateChars(uniqueChars, sameCharObj);
+
+  //return the keys of the object
+  return Object.keys(sameCharObj);
+
 };
