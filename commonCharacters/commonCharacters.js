b133554569b915fefa2a7c19f773239acd243654
raspy8766 <raspy8766@gmail.com>
2015-03-17 09:58:24 -0700

    Completed commonCharacters.

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,7 +10,23 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
+  //if there is a commonCharacter it will be saved to a an object with a prop 
+  //common characters and an array of common characters. Ultimately this array will be 
+  //joined together to create one string.
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  var collection = {};
+  var getComChars = function (endloop, index) {
+    if (endloop === string1.length) {
+      return;
+    };
+    for (var i = 0; i < string1.length; i++) {
+      if( string1[i] === string2[index] && collection[string1[i]] === undefined ) {
+        collection[string1[i]] = true;
+      }
+    };
+    getComChars(endloop+1, index+1);
+  }
+  getComChars(0,0);
+  return Object.keys(collection).join();
 };
