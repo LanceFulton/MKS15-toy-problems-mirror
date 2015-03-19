ca4527335988c0e8ce2245c41818cd7fed443c4a
sperrow <mattjsperry@gmail.com>
2015-03-19 10:01:01 -0700

    Toy problem 3-19-15 * Solution for largestProductOfThree.js

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -1,6 +1,6 @@
 /* Write a function that finds the largest possible product of any three numbers
  * from an array.
- * 
+ *
  * Example:
  * largestProductOfThree([2, 1, 3, 7]) === 42
  *
@@ -9,5 +9,22 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  var largest = 0;
+
+  var recurse = function(current, counter, i) {
+    if(counter === 3) {
+      if(largest < current) {
+        largest = current;
+      }
+      return;
+    }
+
+    for( ; i < array.length; i++ ) {
+      recurse(current * array[i], counter + 1, i + 1);
+    }
+  }
+
+  recurse(1, 0, 0);
+
+  return largest;
 };
