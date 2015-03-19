e3bcc956511b9e12cc96ac78d4f684547d30d37c
Trace Thompson <tracethmpsn@gmail.com>
2015-03-19 10:06:47 -0700

    Finished largestProductOfThree

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,22 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  //hold three largest numbers
+  var results = [];
+
+  //sort array
+  var newArray = array.sort();
+
+  //pop the number from array three times and push it to results
+  for(var i = 0; i < 4; i++){
+    var largest = newArray.pop();
+    if(largest !== undefined){
+    results.push(largest);
+    }
+  }
+
+  //multiply all elements together and return
+  return results.reduce(function(a,b){return a*b;});
+
+  //not working 
 };
