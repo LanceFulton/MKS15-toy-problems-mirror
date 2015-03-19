11579d6f43c9262f052e5603e892f6722d4f4d04
Bryan Lee <bclee@ucdavis.edu>
2015-03-19 09:49:11 -0700

    Solution for LargestProductOfThree

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,25 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
+  var largestNums = [0, 0, 0];
+  var negNums = [0, 0, 0]; // two neg numbers can have a greater total
+  
+  // iterate through array
+  for (var i = 0; i < array.length; i++) {
+    // compare it to the three largest numbers
+    for (var j = 0; j < largestNums.length; j++) {
+      if (array[i] > largestNums[j]) {
+        largestNums.splice(j, 0, array[i]);
+        largestNums = largestNums.slice(0,3);
+        break;
+      } else if (array[i] < negNums[j]) {
+        negNums.splice(j, 0, array[i]);
+        negNums = negNums.slice(0,3);
+        break;
+      }
+    }
+  }
+  // compare all positive vs 1positive and 2negatives
+  return Math.max(largestNums.reduce(function(a,b){ return a * b; }),
+                  largestNums[0] * negNums[0] * negNums[1]);
 };
