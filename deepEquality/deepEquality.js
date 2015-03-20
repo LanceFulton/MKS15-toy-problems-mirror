9c2119b2e3a51e1c0fcdaecf0baa91520792be2e
vladislav-i <saintmz3@gmail.com>
2015-03-20 09:56:47 -0700

    Completed deepEquality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,31 @@
   *
   */
 var deepEquals = function(apple, orange){
+
+  //check each value of key in each object
+  if (apple === orange) {
+    return true;
+  }
+  if (!(Object.getOwnPropertyNames(apple)) || !(Object.getOwnPropertyNames(orange))) {
+    return false;
+  }
+
+  //get all apple and orange keys
+  var appleKeys = Object.keys(apple);
+  var orangeKeys = Object.keys(orange);
+
+  //if there is more keys in apple than orange then its false
+  if (appleKeys.length !== orangeKeys.length) {
+    return false;
+  }
+
+ //use for loop to check each key
+  for (var i = 0; i < appleKeys.length; i++) {
+    if (!deepEquals(apple[appleKeys[i]], orange[appleKeys[i]])) {
+      return false;
+    }
+  }
+
+  return true;
 };
+//deepEquals({a:1, b: {c:5}},{a:1, b: {c:5}});
