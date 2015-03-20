d3ff50da6c04c203cb33ed14ef177db13d67dc67
David Tan <davidtansf@gmail.com>
2015-03-20 10:02:34 -0700

    updated file for deepEquality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,43 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange){
+var deepEquals = function(apple, orange) {
+  var result = true;
+
+  var recursive = function(obj1, obj2) {
+    for (var key in obj1) {
+      if (typeof obj1[key] === "string" || typeof obj1[key] === "number" || typeof obj1[key] === "boolean") {
+        if (obj1[key] !== obj2[key]) {
+          result = false;
+        }
+      }
+      else if (Array.isArray(obj1[key])) { // checks for arrays
+        // iterates through Array
+      }
+      else if (typeof obj1[key] === "function") { // checks for functions
+      }
+      else { //if its an object, use recursion
+        recusive (obj1[key], obj2[key])
+      }
+    }
+  };
+
+
+  recursive (apple, orange);
+
+  return result;
+
 };
+
+
+
+// use for-in loop and use recusion(if needed if objects are found), have to check for strings, numbers, arrays, functions, and objects)
+/*
+
+console.log('numbers: ', 3 === 3); // true
+console.log('string: ', "dd" === "dd"); // true
+console.log('boolean: ', true === true); // true
+console.log([1,2,3] === [1,2,3]); // false
+console.log(myFuncA === myFuncB); //false
+
+*/
