c07dc1c977c6cf24ed315e589270b058ec653684
sperrow <mattjsperry@gmail.com>
2015-03-20 09:58:59 -0700

    Toy problem 3-20-15 * Solution for deepEquality.js

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,32 @@
   *
   */
 var deepEquals = function(apple, orange){
+  if( Object.keys(apple).length !== Object.keys(orange).length ) {
+    return false;
+  }
+
+  var equality = true;
+  var recurse = function(obj1, obj2) {
+    for( var key in obj1 ) {
+      if( key in obj2 ) {
+        // check for nulls
+        if( obj1[key] === null ) {
+          if( obj1[key] !== obj2[key] ) {
+            equality = false;
+          }
+        // check for arrays and objects
+        } else if( typeof obj1[key] === 'object' ) {
+          recurse(obj1[key], obj2[key]);
+        // otherwise compare primitives
+        } else if( obj1[key] !== obj2[key] ) {
+          equality = false;
+        }
+      } else {
+        equality = false;
+      }
+    }
+  };
+
+  recurse(apple, orange);
+  return equality;
 };
