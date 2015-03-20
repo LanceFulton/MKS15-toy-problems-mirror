2cf50f1c04ffa556a04fc22ad208b35c6a803935
raspy8766 <raspy8766@gmail.com>
2015-03-20 10:06:37 -0700

    deepEquality almost finished.

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,44 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+  // {a:1, b: {c:3}}
+
 var deepEquals = function(apple, orange){
+  objectDeepEqual = true;
+
+  var recurse = function(obj1, obj2) {
+    if ( objectDeepEqual === false ) {
+      objectDeepEqual = false;
+      return;
+    };
+    var keys1 = Object.keys(obj1);
+    var keys2 = Object.keys(obj2);
+    if ( keys1.length !== keys2.length ) {
+      objectDeepEqual = false;
+      return;
+    };
+    for (var i = 0; i < keys1.length; i++) {
+      if( keys1[i] !== keys2[i] ) {
+        objectDeepEqual = false;
+        return;
+      };
+      debugger;
+      if( Object.keys(obj1[keys1[i]])[0] !== undefined && Object.keys(obj2[keys2[i]])[0] !== undefined) {
+        recurse(obj1[keys1[i]], obj2[keys2[i]]);
+      }
+      if( typeof(Object.keys(obj1[keys1[i]])) !== 'object' ) {
+        if( obj1[keys1[i]] !== obj2[keys2[i]] ) {
+          objectDeepEqual = false;
+          return;
+        }
+      }
+    };
+
+
+  }
+  recurse(apple, orange);
+
+  return objectDeepEqual;
+
 };
