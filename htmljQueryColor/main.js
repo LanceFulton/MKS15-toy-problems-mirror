ed4d90c47eb56c213d2d49ed2f03b04f7405e276
David Tan <davidtansf@gmail.com>
2015-03-21 10:02:31 -0700

    updated solution set for jQueryColor

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,27 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var pgrab = $('p').text().split(" ");  // gets words from p into array
 
+  //  debugger;
+
+//   for (var i = 0; i < pgrab.length; i++) {
+//     $( "p" ).wrap(function() {
+//   return "<div class='" + $( this ).text() + "'></div>";
+// });
+//   }
+    console.log(pgrab);
+  console.log(pgrab[0]);
+  console.log(pgrab[1]);
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
+function changeColors() {
+  $('p').css('color', 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')')
+};
 
-
+  setInterval(changeColors, 1000)
 });
+
+//couldn't figure out first solution
