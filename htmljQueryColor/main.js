b7e25b80bcc81100b129c8a8ecadaa03661d3820
Thomas Logan <tdloganj@gmail.com>
2015-03-21 09:56:18 -0700

    Mostly Complete problem. Both paragraphs change colors every second. However, I was unable to split up each word and wrap them with a span.

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,21 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  //1 I want to wrap all of the words in the paragraph in span tags. 
 
+
+  $('p').wrap('<span></span>'); //All paragraph elements have span tags now, but words do not have tags. 
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
+  var randomize = function(){
+    var picker = Math.floor(Math.random() * 6);
+    //Set r, o ,y ,g ,b, p to numbers 1-6. Have function pick a random number and match it with 
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    $('span').css('color', colors[picker]);
+  }
 
-
+  setInterval(randomize, 1000);
 
 });
+
