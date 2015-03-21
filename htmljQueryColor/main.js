65da81431780547186907c21edeccf22459b3d0b
Paul Brady <pablo.brady@gmail.com>
2015-03-21 09:58:34 -0700

    jQueryColor:  Attempted

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -5,7 +5,21 @@ $(function(){
   // becomes: <p><span>Hey</span><span>there</span></p>
   // HINT: the `split` array method is your friend
 
-  // TODO: your code here!
+  var pArray = $('p').text();
+  console.log("pArray = " + pArray);
+  //$.contains(pArray, ' ');
+
+  // Exract Paragraphs / words
+  var paragraphArray = [];
+  for (var i = 0; i < pArray.length; i++) {
+    paragraphArray = String(pArray[i]).split(" ");
+    console.log("len = " + paragraphArray[i].length + " paragraph = " + paragraphArray[i]);
+  };
+
+paragraphArray[0] = String(pArray[0]).split(" ").innerHtml;
+  console.log(paragraphArray[0]);
+  // Wrap words in <p>
+
 
 
   // --------------STEP 2--------------
