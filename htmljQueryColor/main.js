8a5731a94395e38b9bd17888a28f9471e03a1e90
Joseph Pei <pei_joseph@yahoo.com>
2015-03-21 09:56:15 -0700

    htmljQueryColor complete

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,20 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var p = $('p');
 
+  for (var i=0; i<p.length; i++) {
+    var parts = p[i].innerHTML.trim().split(' ').join('</span> <span>');
+    $(p[i]).html('<span>'.concat(parts, '</span>'));
+  }
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
-
+  var changeColor = function() {
+    $('span').css('color',function(){return 'rgb('+Math.floor(256*Math.random())+','+Math.floor(256*Math.random())+','+Math.floor(256*Math.random())+')';});
+  };
+  changeColor();
+  setInterval(changeColor, 1000);
 
 
 });
