a642a0fa1dd681b081c144e72310a1049ec269e4
raspy8766 <raspy8766@gmail.com>
2015-03-21 13:16:38 -0700

    Finished jQuery

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -1,4 +1,26 @@
 $(function(){
+  
+  
+  $('p').each(function() {
+    var textArr = $(this).text().split(' ');
+    $(this).empty();
+    for (var i = 0; i < textArr.length; i++) {
+      var span = '<span>' + textArr[i] + ' ' + '</span>';
+      $(this).append(span);
+    };
+    var color = function () {
+      $('span').attr( 'style', function() { return 'color:rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')'})
+    }
+    color();
+    setInterval(color, 1000);
+  })
+
+
+
+
+
+
+
   // --------------STEP 1--------------
   // wrap every word in every `<p>` tag with a `<span>` tag.
   // for example: <p>Hey there</p>
