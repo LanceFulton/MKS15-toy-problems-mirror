c2f6a8125e72a3051fd3e98cc6c568d9b9ed37e6
Thomas Logan <tdloganj@gmail.com>
2015-03-24 09:56:09 -0700

    Significant progress on the bind function. I think I understand it for the most part. I was unable to start the prototype.bind

--- functionBind/functionBind.js
+++ functionBind/functionBind.js
@@ -1,31 +1,31 @@
 /*
- * function bind():
- *
- * example 1:
- *
- * var alice = {
- *   name: 'alice',
- *   shout: function(){
- *     alert(this.name);
- *   }
- * }
- * var boundShout = bind(alice.shout, alice);
- * boundShout(); // alerts 'alice'
- * boundShout = bind(alice.shout, {name: 'bob'});
- * boundShout(); // alerts 'bob'
- *
- * example 2:
- *
- * var func = function(a, b){ return a + b };
- * var boundFunc = bind(func, null, 'foo');
- * var result = boundFunc('bar');
- * result === 'foobar'; // true
- *
-*/
+ function bind():
+  example 1:
+  var alice = {
+   name: 'alice',
+   shout: function(){
+     alert(this.name);
+   }
+ }
+ var boundShout = bind(alice.shout, alice);
+ boundShout(); // alerts 'alice'
+ boundShout = bind(alice.shout, {name: 'bob'});
+ boundShout(); // alerts 'bob'
 
-var bind = function(
-) {
+  example 2:
+  var func = function(a, b){ return a + b };
+ var boundFunc = bind(func, null, 'foo');
+ var result = boundFunc('bar');
+ result === 'foobar'; // true
+ */
+
+var bind = function(cb) {
   // TODO: Your code here
+  var args = [];
+  for (var i = 1; i < arguments.length; i++){//This will create an array of arguments that excludes the initial callback.
+    args.push(arguments[i]);
+  }
+  return cb.apply(this, args);//The idea is to apply all of the arguments to the callback that we are using as our argument.
 };
 
 /*
@@ -53,7 +53,6 @@ var bind = function(
  *
 */
 
-Function.prototype.bind = function(
-) {
+Function.prototype.bind = function() {
   // TODO: Your code here
 };
