f6ed299f017ed1f7eba2ae2671adb9c4da2ddc4e
David Tan <davidtansf@gmail.com>
2015-03-24 09:21:26 -0700

    updated htmljQueryColor with other solution

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -9,10 +9,10 @@ $(function(){
   // gets words from p into array
 
   $('p').each(function(){
-
     $(this).html($(this).html().replace(/[a-zA-Z,.]+/g, "<span>$&</span>"));
     // http://stackoverflow.com/questions/10300122/jquery-to-wrap-certain-word-in-strong-tag
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
 
 
   });
@@ -20,7 +20,7 @@ $(function(){
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
-  function changeColors() { // https://api.jquery.com/each/
+  function changeColors() {
     $('span').each(function() { // each loops through every element and changes into random color
       $(this).css('color', 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')')
     });
@@ -29,4 +29,14 @@ $(function(){
   changeColors();
   setInterval(changeColors, 1000);
 
+});
+
+$('p').each(function(index,element) {
+  var $el = $(element); // grabs the entire paragraph
+  var words = $el.text().split(' '); // creates array with words
+  $el.html(''); // then clear the element since we are going re-add what we need back in
+  for (var i = 0; i < words.length; i++) {
+    var newSpan = $('<span>').text(words[i] + ' ');
+    $el.append(newSpan); // add the span and word back in
+  }
 });
