6ef016b29d8039ff58df17a1d4248d3329d5ed0b
Thomas Logan <tdlogan@Thomass-MacBook-Pro.local>
2015-03-25 09:58:51 -0700

    significant progress

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -18,4 +18,19 @@
  */
 
 var powerSet = function(str){
+  //I want to split the string initially into an array
+  //I also want to create a new array to store our results in
+  //The results array should start with an empty string in the zero index
+  //I finally want to return the array
+  var results = [''];
+  var letters = str.split('');
+  
+  for (var i = 0; i < letters.length; i++){  //----------This will iterate through each letter once until we reach our result.
+    results.push(letters[i]);
+    var removeCurrent = letters.splice(1, letters.length);
+    for (var j = 0; j < removeCurrent.length; j++){
+      results.push(letters[i]+removeCurrent[j]);
+    }
+  }
+  return results;
 }
