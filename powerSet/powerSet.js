f5f90cd78d9f40ee3ae1d52a850acde1b424077e
Bryan Lee <pygmize.me@gmail.com>
2015-03-25 09:57:31 -0700

    Solution to PowerSet

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -17,5 +17,32 @@
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str){
+var powerSet = function(str) {
+  var results = {};
+  var current = [];
+  var strArr = str.split('');
+
+  //for each letter
+    //add a letter, save the result
+    //if there are letters after that
+      //recurse on itself
+  var recursiveBuild = function(strArr) {
+    if (str.length === 1) {
+      var result = current.concat(str).sort().join('');
+      results[result] = true;
+      return;
+    }
+
+    for (var i = 0, len = strArr.length; i < len; i++) {
+      current.push(strArr[i]);
+      var result = current.slice().sort().join('');
+      results[result] = true;
+      if (i !== len - 1) {
+        recursiveBuild(strArr.slice(i + 1));
+      }
+      current.pop();
+    }
+  };
+  recursiveBuild(strArr);
+  return Object.keys(results);
 }
