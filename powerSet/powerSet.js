feb8a9cf8e7a0b6c8ad4e643f5e493fd98d7ba0c
Paul Brady <pablo.brady@gmail.com>
2015-03-25 09:57:35 -0700

    PowerSet:  Attempted

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -18,4 +18,30 @@
  */
 
 var powerSet = function(str){
+  console.log("---> " + str);
+  var pSet = [];
+  var word = "";
+
+  // Store all combinations of "str" in pSetObj (excluding flipped-order duplicates)
+  // "", Letter1, Letter2, Letter3, (skip Letter1Letter1), Letter1Letter2, Letter1Letter3
+
+  var collector = function(out, pos) {
+    if(pos>str.length - 1) return pSet;
+    word = "";
+    for (var i = pos; i<str.length; i++) {
+      word += str[i];
+      pSet.push(word);
+      for (var k = i; k<str.length; k++) {
+        collector(pSet, k);
+      }
+    }
+
+    return pSet
+  }
+
+  return collector(pSet, 0, 1);
+
 }
+
+console.log( powerSet("abc") );
+// powerSet("jump");
