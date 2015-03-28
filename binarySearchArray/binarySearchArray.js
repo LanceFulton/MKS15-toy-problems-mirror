3a42e0104afbd423b5d131a34b3be7de47a4bdb6
Bryan Lee <pygmize.me@gmail.com>
2015-03-28 09:33:00 -0700

    solution for binarySearchArray.js

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,5 +9,21 @@
  */
 
 var binarySearch = function (array, target) {
-};
+  var start = 0;
+  var end = array.length - 1;
+  var middle;
 
+  if ( target < array[start] || target > array[end] ) {
+    return -1;
+  }
+
+  while ( (array[ middle = Math.floor((start + end) / 2) ]) !== target ) {
+    if ( target < array[middle] ) {
+      end = middle;
+    } else if ( target > array[middle] ) {
+      start = middle;
+    }
+  }
+
+  return middle;
+};
