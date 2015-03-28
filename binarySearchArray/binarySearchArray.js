5ab421685bfeaf2024fb6977e07e194e03320f29
Joseph Pei <pei_joseph@yahoo.com>
2015-03-28 09:21:58 -0700

    binarySearchArray complete

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,5 +9,22 @@
  */
 
 var binarySearch = function (array, target) {
+  var binarySearchRecurse = function(low, high) {
+    if (low < high) {
+      var med = Math.floor((low + high)/2);
+      if (array[med] === target) {
+        return med;
+      } else {
+        if (array[med] < target) {
+          return binarySearchRecurse(med+1, high);
+        } else {
+          return binarySearchRecurse(low, med-1);
+        }
+      }
+    } else return -1;
+  }
+  return binarySearchRecurse(0, array.length-1);
 };
 
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index); // [3]
