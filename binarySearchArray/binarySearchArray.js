ae9b968112153b448b0e279e24343b728b256077
Thomas Logan <tdlogan@Thomass-MacBook-Pro.local>
2015-03-28 09:56:13 -0700

    Significant progress. I was able to understand what to do, but it took me too long to figure that out so I ran out of time.

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,5 +9,21 @@
  */
 
 var binarySearch = function (array, target) {
+  //I need to find a way to go to the lowest point on the tree
+  currentIndex = 0;
+
+  var nextNode = function(node) {
+    
+    if (node === target) { return '[' + currentIndex + ']'; };//returns the current node!
+    while (var i = 0; i < node.children.length){
+      if (target === node.children[i]) { //Will move down further on the tree on the left if it is lower than the current node.
+        currentIndex++;
+        return '[' + currentIndex + ']';
+      } else if (target > node.children[i]) {
+        currentIndex++;
+        nextNode(node.children[i]);
+      }
+    } 
+  }
 };
 
