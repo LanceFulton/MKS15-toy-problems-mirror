2e9bb928dc1d9f6a414cde96e2e49ae7aaf452f9
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-03-28 09:39:58 -0700

    3/28 Toy Problem Without Cheating

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -8,8 +8,29 @@
  * console.log(index); // [3]
  */
 
+//It has come to my attention that this might be cheating. 
 var binarySearch = function (array, target) {
   var index = array.indexOf(target);
   return index;
 };
 
+
+var binarySearch = function(array, target) {
+  var midPointIndex = Math.floor((array.length-1) / 2 );
+
+  if (target === array[midPointIndex]) {
+    return midPointIndex;
+  }
+
+  if (target > array[midPointIndex])
+  for (var i = midPointIndex + 1 ; i < array.length; i++) {
+    if (array[i] === target)
+      return i;
+  }
+
+  if (target < array[midPointIndex])
+  for (var j = midPointIndex - 1 ; j < array.length; j--) {
+    if (array[j] === target)
+      return j;
+  }
+};
