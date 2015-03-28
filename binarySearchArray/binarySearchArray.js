7a88c3cbf662d4d2918f71714a40e577d9851d26
David Tan <davidtansf@gmail.com>
2015-03-28 13:21:22 -0700

    updated file

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -1,17 +1,6 @@
-/*
- * Given a sorted array, find the index of an element
- * using a binary search algorithm.
- *
- * Example usage:
- *
- * var index = binarySearch([1, 2, 3, 4, 5], 4);
- * console.log(index); // [3]
- */
-
-
 var binarySearch = function (array, target) {
 
-  var indextracker = 0;
+  var indextracker = array.length;
   var found = false;
 
   var recursive = function (array, target) {
@@ -19,7 +8,7 @@ var binarySearch = function (array, target) {
     var midpoint = Math.floor(array.length/2);
 
     if (target === array[midpoint]) {  // if target is found
-      indextracker += Math.floor(array.length/2);
+      indextracker -= Math.floor(array.length/2+1); // ceiling or floor
       found = true;
     }
 
@@ -28,14 +17,15 @@ var binarySearch = function (array, target) {
     }
 
     else if (array[midpoint] < target) { // if target is higher than midpoint
-
-
-    // recursive (newArray, target)
+      var newArray = array.slice(midpoint, array.length+1)
+      indextracker += -Math.ceil(array.length/2);
+      recursive(newArray, target);
     }
 
     else if (target < array[midpoint]) { // if target is lower than midpoint
-      var newArray = array.slice(midpoint);
-      indextracker += Math.floor(array.length/2);
+      var newArray = array.slice(0, midpoint);
+     // console.log('newARRAY: ', newArray);
+      indextracker -= Math.ceil(array.length/2-1);
       recursive(newArray, target);
     }
 
@@ -66,3 +56,10 @@ console.log(index); // null
 
 var index = binarySearch([1, 3], 1);
 console.log(index); // 0
+
+var index = binarySearch([1,2,3,4,5,6,7,8,9,10], 2);
+console.log(index); // 1
+
+var index = binarySearch([1,2,3,4,5,6,7,8,9,10], 9);
+console.log(index); // 8
+
