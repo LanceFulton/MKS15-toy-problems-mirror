210dea221d32c0d1f5c63d1e558e07791c2d6ca3
Ben Pevsner <pevsner.ben@gmail.com>
2015-03-28 09:53:13 -0700

    incomplete binarySearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -6,8 +6,40 @@
  *
  * var index = binarySearch([1, 2, 3, 4, 5], 4);
  * console.log(index); // [3]
+ [1,2,3,4,5]
+ 3
+
+ [4,5]
+
+ [4]
  */
 
 var binarySearch = function (array, target) {
+	var result = 0;
+
+  var subroutine = function(arrayLength, index, target){  
+	  
+	  var middle = Math.floor((arrayLength+1)/2);
+
+	  if(index === target){
+
+	  	return index + result;
+
+	  }else if(target > index){
+      result = result + (arrayLength/4) * 3;
+
+	  	return binarySearch(index + (arrayLength, target);
+
+	  }else if(target < index){
+      result = result + (arrayLength/4);
+
+	  	return binarySearch(index,  target);
+
+	  }
+  }
+
+  return subroutine(array.length, Math.floor(array.length/2), target);
+
 };
 
+console.log(binarySearch([1,2,3,4,5], 4));
