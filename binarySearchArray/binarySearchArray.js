23814a3444c678c5b873ab44a4b27d90c88be1fa
Seven Li <sevenlist0110@gmail.com>
2015-03-28 09:49:47 -0700

    Finished binarysearchArray

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,5 +9,32 @@
  */
 
 var binarySearch = function (array, target) {
+  var minIndex=0;
+  var maxIndex=array.length-1;
+  var midIndex;
+  while (minIndex<=maxIndex){
+    midIndex=Math.round((minIndex+maxIndex)/2);
+    if (array[midIndex]=== target){
+      return midIndex;
+    }
+    else if (array[midIndex]< target) {
+      minIndex=midIndex;
+      midIndex=Math.round((midIndex+maxIndex)/2);
+      maxIndex--;
+      if (minIndex>maxIndex){
+        break;
+      }
+    }
+    else {
+      maxIndex=midIndex;
+      midIndex=Math.round((minIndex+midIndex)/2);
+      minIndex++;
+      if (minIndex>maxIndex){
+        break;
+      }
+    }
+  }
+  return -1;
 };
+
 
