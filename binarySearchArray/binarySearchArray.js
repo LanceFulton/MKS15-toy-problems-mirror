8df0afd0fb0de02546f922fb4a4a15be7c0c1d07
sperrow <mattjsperry@gmail.com>
2015-03-28 09:50:13 -0700

    Solution for binarySearchArray.js

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,5 +9,19 @@
  */
 
 var binarySearch = function (array, target) {
-};
+  var mid = Math.floor(array.length / 2);
 
+  var halve = function(array, target, midpoint) {
+    if( target === array[midpoint] ) { return midpoint; }
+    else if( target < array[midpoint] ) {
+      midpoint = Math.floor(midpoint / 2);
+    } else if( target > array[midpoint] ) {
+      midpoint = Math.floor(midpoint + midpoint / 2);
+    }
+    return halve(array, target, midpoint);
+  };
+
+  if( target === array[0] ) { return 0; }
+  else if( target === array[array.length - 1] ) { return array.length - 1; }
+  else {  return halve(array, target, mid); }
+};
