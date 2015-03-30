f307ad0fc18f8b387b1aae93d7aa495a60eafd7f
vladislav-i <saintmz3@gmail.com>
2015-03-29 22:07:56 -0700

    fixed binary search

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -7,10 +7,22 @@
  * var index = binarySearch([1, 2, 3, 4, 5], 4);
  * console.log(index); // [3]
  */
-
 var binarySearch = function (array, target) {
-    return (array.indexOf(target)); 
-};
+   
+   var recurse = function(array,target, start, end){
+       console.log(start, end);
+    var delta = Math.round((end-start)/2); //rounds it up
+    var idx = start+delta;
+  console.log(array[idx]);
+    if (array[idx]<target) {
+        return recurse(array,target, idx+1, end);
+        
+    } else if(array[idx]>target){
+        return (recurse(array,target, start, idx-1));
+    } else {
+        return idx;
+    }
+   };
+   return recurse(array, target, 0, array.length-1);
 
-var index = binarySearch([1, 2, 3, 4, 5], 4);
-console.log(index); //3
+};
