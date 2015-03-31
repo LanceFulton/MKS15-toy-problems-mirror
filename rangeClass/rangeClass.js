9204ab69c19062650da1b205783db8d24c7811fe
Joseph Pei <pei_joseph@yahoo.com>
2015-03-31 09:57:23 -0700

    rangeClass complete

--- rangeClass/rangeClass.js
+++ rangeClass/rangeClass.js
@@ -38,16 +38,54 @@
 
 
 var Range = function(start, end, step) {
+  if (start == null) {
+    return null;
+  }
+  if (end == null) {
+    end = start;
+  }
+  if (!step) {
+    step = end >= start ? 1 : -1;
+  }
+  this.start = start;
+  this.end = end;
+  this.step = step;
 };
 
 Range.prototype.size = function () {
+  return Math.max(0,Math.floor((this.end-this.start)/this.step)+1);
 };
 
 Range.prototype.each = function (callback) {
+  var endTest = function(current, context) {
+    if (context.step>0) {
+      return current > context.end;
+    } else {
+      return current < context.end;
+    }
+  };
+  for (var current = this.start; !endTest(current, this); current+=this.step) {
+    callback(current);
+  }
 };
 
 Range.prototype.includes = function (val) {
+  var stepsToVal = (val - this.start) / this.step;
+  var stepsToEnd = (this.end - this.start) / this.step;
+  // Return is in stepping range and integral
+  return stepsToVal >= 0 && stepsToVal <= stepsToEnd && Math.floor(stepsToVal) === stepsToVal;
 };
 
 var range = new Range(1);
 
+// var myRange = new Range(0,10); // a new range representing the numbers between 0 and 10 (inclusively)
+
+// var evenNumbers = new Range(2,8,2); // A range with the even numbers 2, 4, 6, and 8.
+// evenNumbers.each(function(val){
+//   console.log(val+"!");
+// });
+// console.log("Who do we appreciate!?");
+
+// console.log(evenNumbers.size()); // should be 4
+// console.log(evenNumbers.includes(2)); // should be true
+// console.log(evenNumbers.includes(3)); // should be false
