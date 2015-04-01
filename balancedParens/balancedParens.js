ec2503685c9c8724fc2144751ba148c8e2d65085
sperrow <mattjsperry@gmail.com>
2015-04-01 10:01:52 -0700

    Partial solution for balancedParens.js

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,64 @@
  *
  */
 var balancedParens = function(input){
+  var openParens = false;
+  var countParens = 0;
+  var openBrackets = false;
+  var countBrackets = 0;
+  var openCurly = false;
+  var countCurly = 0;
+
+  for( var i = 0; i < input.length; i++ ) {
+    if( '[](){}'.indexOf(input[i]) >= 0) {
+      if( input[i] === '(' ) {
+        if( !openParens ) {
+          openParens = true;
+        } else {
+          countParens++;
+        }
+      } else if( input[i] === '[' ) {
+        if( !openBrackets ) {
+          openBrackets = true;
+        } else {
+          countBrackets++;
+        }
+      } else if( input[i] === '{' ) {
+        if( !openCurly ) {
+          openCurly = true;
+        } else {
+          countCurly++;
+        }
+      } else if( input[i] === ')' ) {
+        if( countParens > 0 ) {
+          countParens--;
+        } else if( openParens ) {
+          openParens = false;
+        } else {
+          return false;
+        }
+      } else if( input[i] === ']' ) {
+        if( countBrackets > 0 ) {
+          countBrackets--;
+        } else if( openBrackets ) {
+          openBrackets = false;
+        } else {
+          return false;
+        }
+      } else if( input[i] === '}' ) {
+        if( countCurly > 0 ) {
+          countCurly--;
+        } else if( openCurly ) {
+          openCurly = false;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  if( !openParens && !openBrackets && !openCurly && countParens === 0 && countBrackets === 0 && countCurly === 0 ) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
-
