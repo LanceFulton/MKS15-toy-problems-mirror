de7f048091bf02993b95a5fc5a3b8c15a12fc4b6
travisjn <tdizzletrav@gmail.com>
2015-04-01 09:58:51 -0700

    Progress, ran out of time

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,76 @@
  *
  */
 var balancedParens = function(input){
+  //prob want to recurse
+  //split string into array
+  //counter that counts open parens
+  //decrememnt counter as it comes across a closed paren
+
+  var array = input.split('');
+  var openCount = 0;
+
+  var openChars = {
+    '(': 0,
+    '[': 0,
+    '{': 0
+  };
+
+  var closedChars = {
+    ')': 0,
+    ']': 0,
+    '}': 0
+  };
+
+  //need to keep track of each type of bracket
+  //should use an object with bracket as key, counter as value
+  //compare the open values with closed values to see if they match
+
+  for(var i = 0; i < array.length; i++){
+
+    if(openChars[array[i]] !== undefined){
+      openChars[array[i]] += 1;
+    } else if (closedChars[array[i]] !== undefined){
+      closedChars[array[i]] += 1;
+    }
+  }
+  //still need to capture edge cases, a bracket that closes without first opening
+  //brackets that aren't closed within closed parens
+  //out of time
+
+  if(openChars['('] !== closedChars[')']){
+    return false;
+  } else if (openChars['['] !== closedChars[']']){
+    return false;
+  } else if (openChars['{'] !== closedChars['}']){
+    return false;
+  }
+
+  return true;
+
 };
 
 
+//Code below will work for all combos of parens
+
+// var array = input.split('');
+//   var openCount = 0;
+
+
+//   for(var i = 0; i < array.length; i++){
+
+//     if(array[i] === '('){
+//       openCount++;
+//     } else if (array[i] === ')'){
+//       if(openCount <= 0){
+//         return false;
+//       } else {
+//         openCount--;
+//       }
+//     }
+//   }
+
+//   if(openCount !== 0){
+//     return false;
+//   }
+
+//   return true;
