ac9278e3a062bea5c6cc5aa7805f1e19dc462083
Paul Brady <pablo.brady@gmail.com>
2015-04-02 09:45:50 -0700

    Async Map: Attempted

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -40,4 +40,35 @@
 
 
 var asyncMap = function(tasks, callback){
+
+  var taskList = [];
+  var taskResults = [];
+
+  for (var i = 0; i < tasks.length; i++) {
+    taskList.push( callback )
+  };
+  console.log("taskList: " + taskList);
+  for (var k = 0; k <taskList.length; k++) {
+    callback(taskList[k]);
+  }
 };
+
+
+asyncMap([
+ function(cb){
+     setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+ ],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
+
