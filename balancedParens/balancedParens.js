7a14157e35c329193d92111979931d45be84fa8e
LanceFulton <lsfaccounts@gmail.com>
2015-04-03 08:41:35 -0700

    balancedParens started.

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,80 @@
  *
  */
 var balancedParens = function(input){
+
+  var lastParen = '(';
+  // set counts
+  var changeCount = 0;
+  var openCount = 0;
+  var closedCount = 0;
+
+  // iterate through characters
+  for (var i = 0 ; i < input.length ; i++){
+    var char = input.charAt(i);
+
+    // if changeCount > 1, return false
+    if (changeCount > 1){
+      return false;
+    }
+
+    // check if character matches lastParen
+    if (char === lastParen){
+      // if so, increment type count
+      if (char === ')'){
+        openCount++;
+      } else {
+        closedCount++;
+      }
+      // if not, change lastParen, increment change count, and increment type count
+    } else {
+      if (lastParen === '('){
+        lastParen = ')';
+      } else {
+        lastParen = '(';
+      }
+      changeCount++;
+    }
+
+
+
+  }
+
+  // if openCount and closedCount don't match, return false
+
+  return true;
+
 };
 
+// var balancedParens = function(input){
+
+//   // set nextParen value to open
+//   var nextParen = '(';
+
+//   // iterate through characters
+//   for (var i = 0 ; i < input.length ; i++){
+//     var char = input.charAt(i);
+
+//     // if the character is a parenthesis
+//     if (char === ')' || char === '('){
+
+//       // check if it matches the nextParen value
+//       if (char === nextParen){
+//         // if so, change nextParen value
+//         if (nextParen === '('){
+//           nextParen = ')';
+//         } else {
+//           nextParen = '(';
+//         }
+//         // if not, return false
+//       } else {
+//         return false;
+//       }
+
+//     }
+
+//   }
+
+//   return false;
+
+// };
 
