2c38e49ff44b86d0d0389ebcece5a8d3512f01b4
sperrow <mattjsperry@gmail.com>
2015-04-03 09:59:48 -0700

    Solution for longestRun.js

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,7 +12,28 @@
  */
 
 var longestRun = function (string) {
-  // TOD: Your code here!
+  var start = 0, lRun = 0, lStart = 0, lEnd = 0;
+
+  for( var i = 1; i < string.length; i++ ) {
+    if( string[i] !== string[start] ) {
+      var end = i - 1;
+      if( lRun < end - start ) {
+        lRun = end - start;
+        lStart = start;
+        lEnd = end;
+      }
+      start = i;
+    } else if( i === string.length - 1) {
+      var end = i;
+      if( lRun < end - start ) {
+        lRun = end - start;
+        lStart = start;
+        lEnd = end;
+      }
+    }
+  }
+
+  return [lStart, lEnd];
 };
 
 // If you need a random string generator, use this!
