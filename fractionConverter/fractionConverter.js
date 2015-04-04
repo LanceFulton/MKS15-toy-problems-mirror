e210868d523fd3288d699a8ce107d46bb8cfaaaa
Thomas Logan <tdlogan@Thomass-MacBook-Pro.local>
2015-04-04 09:56:28 -0700

    Significant progress. I can pass tests for whole numbers and X.5 numbers. However I couldn't produce a solution that takes in all other decimal numbers.

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -12,6 +12,27 @@
  *
  */
 
+
 var toFraction = function(number) {
   // Your code here
+  //I need to take a look at the intial fraction thats being passed in. 
+  //if whole number just divide by one
+  //if X.5 then we know it can be divided by two. 
+  var num1, num2;
+  var remain = 0;
+  if (number.floor !== 0){
+    remain = number % Math.floor(number)).toFixed1;//Getting the remainder of the initial number
+  }
+  
+
+
+  if (number % 0.5 === 0) {//Checks for numbers divisible by two
+    num1 = Math.floor(number) * 2 + 1;
+    num2 = 2
+  } 
+  if (number === Math.round(number)) {//Checks for whole numbers and produces irregular fractions.
+    num1 = number; 
+    num2 = 1;
+  }
+  return num1 + '/' + num2;
 };
