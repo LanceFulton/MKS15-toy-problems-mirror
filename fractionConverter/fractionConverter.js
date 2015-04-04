cd8230613c96f369969a8fb37dc1dc5193b17017
Seven Li <sevenlist0110@gmail.com>
2015-04-04 09:39:52 -0700

    Finished fractionConverter

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -14,4 +14,21 @@
 
 var toFraction = function(number) {
   // Your code here
+  var arr = number.toString().split('.');
+  var whole = +arr[0];
+  if(!arr[1]) {
+    arr[1]=0;
+  }
+  var deci = +arr[1];
+  var denom = (deci === 0) ? 1 : 10/deci;
+  var numerator = denom*whole+ ((deci === 0) ? 0:1) ;
+  return numerator + "/" + denom;
+
 };
+
+/*
+console.log(toFraction(0.5));
+console.log(toFraction(3.0));
+console.log(toFraction(2.5));
+console.log(toFraction(4.2));
+*/
