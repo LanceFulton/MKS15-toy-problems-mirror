767b0166630f72a6c02d2f31f316fce76e948de1
David Tan <davidtansf@gmail.com>
2015-04-04 09:17:02 -0700

    added optimized solution for longestRun

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -36,6 +36,28 @@ var longestRun = function(string) {
   return results = [begin, end];
 };
 
+/*
+
+ Optimizied:
+
+var longestRun = function(string) {
+  var max = [0,0];
+  var current = [0,0];
+  for (var i = 1; i < string.length; i++) {
+    if (string[i-1] === string[i]) {
+      current[1] = i;
+      if (current [1] - current [0] > max[1] - max[0]) {
+        max = current;
+      }
+    } else {
+      current = [i,i];
+    }
+  }
+  return max;
+};
+
+*/
+
 // If you need a random string generator, use this!
 // (you wont need this function for your solution but it may help with testing)
 var randomString = function (len) {
