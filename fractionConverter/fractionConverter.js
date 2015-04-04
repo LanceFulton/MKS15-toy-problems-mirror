c555466c3cfeb3a68a55de135c0d7396986a5e2b
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-04-04 09:58:10 -0700

    4/4 Toy Problem

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -13,5 +13,26 @@
  */
 
 var toFraction = function(number) {
-  // Your code here
+  var x = 1;
+  var y = 1;
+
+  if (number % 1 === 0) {
+    x = number;
+    return fraction;
+  }
+
+
+  var array = number.toString().split('.');
+
+  console.log(array);
+
+  if (parseInt(array[0]) !== 0) {
+    console.log(parseInt(array[0]));
+    // x = (array[1] / array[0]);  
+    // y = array [1] / 2;
+  }
+
+
+  var fraction = x + '/' + y;
+  return fraction;
 };
