a70370bae50e64cb203d817a3fe02c10f99c6ada
Sean Conroy <conroysean@gmail.com>
2015-04-04 21:25:36 -0700

    complete solution

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -15,26 +15,31 @@
 // Had issues getting 3.0 to a string.. decided to add an if statement - wasted lots of time on what was a v. simple answer.
 
 var toFraction = function(number) {
+  var gcd = function(a, b) {
+    if ( ! b) {
+      return a;
+    }
+    return gcd(b, a % b);
+  };
   if (number % 1 === 0){
-    wholeNum = number+'/1'
+    wholeNum = number+ '/1';
     return wholeNum;
   } else {
-    var numberString = number+'';
+    var result;
+    var numberString = number+ '';
     var digits = numberString.split('');
     var decimal = digits.indexOf('.');
-    var result = [];
-    console.log(decimal);
-    if (decimal > 0){
-      var integers = digits.slice(0, decimal);
-      var base = integers * 10; //*denominator?
-    }
-    var decimals = digits.slice(decimal+1, digits.length)
-    console.log('fractions', fractions)
-    // loop through decimals[i]/10 + i/100 + i/1000 + i/10000 ... etc+ 1/100000 + 9/1000000
-    // at end of loop, determine denominator, factor in the base x denominator.
-  }
-};
+    var decimalDigits = digits.slice(decimal+1, digits.length);
+    var magnitudeDeci = decimalDigits.length;
+    var decimals = decimalDigits.join('');
+    var denominator = Math.pow(10, magnitudeDeci);
+    var integers = digits.slice(0, decimal);
+    var magnitudeInt = integers.length;
+    integers = parseInt(integers.join(''));
+    var intNumerator = (integers*denominator + parseInt(decimals));
+    var gcf = gcd(intNumerator, denominator);
+    return ((integers*denominator/gcf) + decimals/gcf + '/' + denominator/gcf);
+}
+}
 
-
-toFraction(3.3);
 
