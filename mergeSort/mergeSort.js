39df77912b1d3058a365445d51384ab9ade3ba13
Trace Thompson <tracethmpsn@gmail.com>
2015-04-07 09:48:25 -0700

    mergsort done

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -98,5 +98,35 @@
 
 
 var mergeSort = function(array) {
-  // Your code here.
+  var middle = parseInt(arr.length / 2);
+  var left   = arr.slice(0, middle);
+  var right  = arr.slice(middle, arr.length);
+  
+  if(arr.length < 2){
+    return arr;
+  }
+  return merge(mergeSort(left), mergeSort(right));
+}
+ 
+function merge(left, right) {
+  var result = [];
+
+  while(left.length && right.length){
+    if(left[0] <= right[0]) {
+      result.push(left.shift());
+    } 
+    else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length){
+    result.push(left.shift());
+  }
+
+  while (right.length){
+    result.push(right.shift());
+  }
+
+  return result;
 };
