86ebbe96113c7ed31ef062ef5f149906a4f394b8
Trace Thompson <tracethmpsn@gmail.com>
2015-04-21 09:57:30 -0700

    linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -19,21 +19,53 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.list = {}
+  this.list.head = null;
+  this.list.tail = null;
 };
 
-//write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  var newNode = LinkedList.prototype.makeNode(value);
+  if(this.list.head === null){
+    this.list.head = newNode
+    this.list.tail = newNode
+  } 
+  else {
+    this.list.tail = newNode //need to fix this
+    this.list.tail.next = newNode
+    console.log("list: ", this.list)
+  }
 };
 
 LinkedList.prototype.removeHead = function(){
+  if(this.list.head === null){
+    return null;
+  }
+  else{
+    var result = this.list.head.value;
+    this.list.head = list.head.next;
+    return result;
+  }
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(target){ 
+  var currentNode = this.list.head;
+  console.log("current: ", currentNode)
+  while(currentNode !== null){
+    if(currentNode.value === target){
+      return true;
+    }
+    else{
+      currentNode = currentNode.next;
+    }
+  }
+  return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
 };
