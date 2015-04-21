b23ed72ca234d73be30af2b508233b6cd8654000
Paul Brady <pablo.brady@gmail.com>
2015-04-21 09:36:45 -0700

    All but 'makeNode' complete.

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,22 +18,49 @@
 
 
 var LinkedList = function(){
-  //fill me in!
+  this.head = null;
+  this.tail = null;
+  this.storage = [];
+
 };
 
-//write methods here!
+LinkedList.prototype.addToTail = function(element){
+  if(this.storage.length==0) { this.head = element; }
 
-LinkedList.prototype.addToTail = function(
-){
+  this.storage[this.storage.length] = element;
+  console.log("addToTail:  Storage = " + this.storage);
+  this.tail = element;
 };
 
 LinkedList.prototype.removeHead = function(){
+  //console.log("a) this.head = " + this.head);
+  if(!this.head) { return; }
+  this.storage.shift();
+  this.head = this.storage[0];
+  //console.log("b) this.head = " + this.head);
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(element){
+  // Search this.storage for element
+  if(this.storage.indexOf(element)>=0) {
+    return true;
+  }
+  return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(node){
+  // 
+
 };
+
+
+var list = new LinkedList();
+// list.tail.value;   //yields 'null'
+list.addToTail(4);
+list.addToTail(5);
+console.log("HEAD: " + list.head);   //yields '4';
+console.log("Contains5: " + list.contains(5));  //yields 'true';
+console.log("Contains6: " + list.contains(6));  //yields 'false';
+list.removeHead(); //yields '4'
+console.log(list.tail);   //yields '5';
+
