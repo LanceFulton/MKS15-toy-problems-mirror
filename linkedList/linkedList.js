e6cd1f098ce0576057791ba8114f9ff00ea1ac53
sperrow <mattjsperry@gmail.com>
2015-04-21 09:48:37 -0700

    Solution for linkedList.js

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,6 +1,6 @@
 /*
  * Implement a linked list using the pseudoclassical instantiation pattern.
- * 
+ *
  * Your linked list should have methods called "addToTail", "removeHead", and "contains."
  *
  */
@@ -19,21 +19,46 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.head = this.makeNode(null);
+  this.tail = this.makeNode(null);
+  this.head.prev = this.tail;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  var newTail = this.makeNode(value);
+  if( !this.head.value ) {
+    this.head = newTail;
+    this.tail = newTail;
+  } else {
+    this.tail.prev = newTail;
+    this.tail = newTail;
+  }
 };
 
 LinkedList.prototype.removeHead = function(){
+  var oldHead = this.head;
+  this.head = this.head.prev;
+  return oldHead.value;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(value){
+  var node = this.head;
+  do {
+    if( node.value === value ) {
+      return true;
+    } else if( node.prev ){
+      node = node.prev;
+    } else {
+      return false;
+    }
+  } while( node );
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+  return {
+    value: value,
+    prev: null
+  };
 };
