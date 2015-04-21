5b4057ae8b7d0c4d1ab9ce82373314e5a03bbe51
Seven Li <sevenlist0110@gmail.com>
2015-04-22 00:56:03 +0800

    complete linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -17,23 +17,69 @@
 // list.tail.value;   //yields '5';
 
 
+var node = function(value) {
+  this.value = value;
+  this.next = null; 
+}
+
 var LinkedList = function(){
-  //fill me in!
+  //fill me in
+  var startNode = new node();
+  this.head = startNode;
+  this.tail = startNode;
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+  var newTail = new node(value);
+
+    if (!this.head || this.head.value === undefined) {
+      this.head = newTail;
+    }
+
+    if (this.tail) {
+      this.tail.next = newTail;
+    }
+
+    this.tail = newTail;
 };
 
 LinkedList.prototype.removeHead = function(){
+  if (this.head === null){
+      return null;
+    }
+
+    var currentHead = this.head;
+    this.head = this.head.next;
+
+    return currentHead.value;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(target){
+  var node = this.head;
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      }
+
+      node = node.next;
+    }
+
+    return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(){
 };
+
+
+ var list = new LinkedList();
+ console.log(list.tail.value);   //yields 'null'
+ list.addToTail(4);
+ list.addToTail(5);
+ console.log(list.head.value);   //yields '4';
+ console.log(list.contains(5));  //yields 'true';
+ console.log(list.contains(6));  //yields 'false';
+ console.log(list.removeHead()); //yields '4'
+ console.log(list.tail.value);  // 5
