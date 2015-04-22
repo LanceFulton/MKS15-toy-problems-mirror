d3247d40d0d5e16ae4387563e4d99865927e87a5
Paul Brady <pablo.brady@gmail.com>
2015-04-22 09:56:44 -0700

    [Attempted] Tree

--- tree/tree.js
+++ tree/tree.js
@@ -9,16 +9,43 @@ Your tree should have methods named "addChild" and "contains".
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value){
-  //tree code goes here!
+  this.tree = null;
+  if(value!==null) {
+    value.left = null;
+    value.right = null;
+    this.tree = value;
+  }
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value){
+  value.left = null;
+  value.right = null;
+  
+  var processInsert = function(treeValue, value) {
+    while(this.tree!==null && ) {
+      if (this.tree.left > value) {
+        processInsert(treeValue, value);
+      }
+    }
+  }
+  processInsert(value);
+
+  // Loop thru children of parent.
+
+
+  // Follow children if not null;
+
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value){
+
 };
+
+
+var tree = treeMaker();
+tree.addChild(1);
+tree.addChild(2);
+tree.contains(2);   // yields 'true'
