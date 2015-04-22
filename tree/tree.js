03454f519c0f744ad0819f9824c75dccc3a46339
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-04-22 09:43:32 -0700

    4/22 Toy Problem

--- tree/tree.js
+++ tree/tree.js
@@ -9,16 +9,29 @@ Your tree should have methods named "addChild" and "contains".
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value){
-  //tree code goes here!
+  var tree = Object.create(methods);
+  tree.value = value;
+  tree.children = [];
+  return tree;
 };
 
-//methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value){
+  var newTree = this.treeMaker(value);
+  this.tree.children.push(newTree);
+
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value){
+  if (tree.value === value) {
+    return true;
+  } else if (tree.children.length > 0) {
+    for (var i = 0; i < tree.children.length; i++) {
+      if (tree.children[i].value === value) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
