74772f49bfe5a7a8482b8f33107a521d81c4be00
sperrow <mattjsperry@gmail.com>
2015-04-22 09:46:21 -0700

    Solution for tree.js

--- tree/tree.js
+++ tree/tree.js
@@ -10,15 +10,34 @@ Your tree should have methods named "addChild" and "contains".
 
 var treeMaker = function(value){
   //tree code goes here!
+  var tree = Object.create(treeMaker.methods);
+  tree.value = value;
+  tree.children = [];
+  return tree;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value){
+  var tree = new treeMaker(value);
+  this.children.push(tree);
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value){
+  var found = false;
+
+  var recurse = function(tree, value) {
+    if( tree.value === value ) {
+      found = true;
+      return;
+    } else if( tree.children.length > 0 ) {
+      for( var i = 0; i < tree.children.length; i++ ) {
+        recurse(tree.children[i], value);
+      }
+    }
+  };
+
+  recurse(this, value);
+  return found;
 };
