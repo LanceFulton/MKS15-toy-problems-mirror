355fe0c5b2a6eaa5ac5606d4070152ce1611a462
LanceFulton <lsfaccounts@gmail.com>
2015-04-22 09:43:22 -0700

    Tree complete.

--- tree/tree.js
+++ tree/tree.js
@@ -9,16 +9,27 @@ Your tree should have methods named "addChild" and "contains".
 // tree.contains(2);   // yields 'true'
 
 var treeMaker = function(value){
-  //tree code goes here!
+  var result = Object.create(treeMaker.methods);
+  result.children = [];
+  result.value = value;
+  return result;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value){
+  var newTree = treeMaker(value);
+  this.children.push(newTree);
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value){
+  for (var i = 0 ; i < this.children.length ; i++){
+    if (this.children[i].value === value){
+      return true;
+    } else {
+      this.children[i].contains(value)
+    }
+  }
+  return false;
 };
