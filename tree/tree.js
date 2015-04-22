701c4544436051eaa66766d6e75d1a8fb0b95308
Bryan Lee <pygmize.me@gmail.com>
2015-04-22 09:44:58 -0700

    Solution to tree

--- tree/tree.js
+++ tree/tree.js
@@ -8,17 +8,32 @@ Your tree should have methods named "addChild" and "contains".
 // tree.addChild(2);
 // tree.contains(2);   // yields 'true'
 
-var treeMaker = function(value){
-  //tree code goes here!
+var treeMaker = function(value) {
+  var tree = Object.create(treeMaker.methods);
+
+  tree.children = [];
+  tree.value = value;
+
+  return tree;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value) {
+  this.children.push(treeMaker(value));
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(value)) {
+      return true;
+    }
+  }
+
+  return false;
 };
