914b9b4592d48e10a112e7787921d20c24950f9d
Seven Li <sevenlist0110@gmail.com>
2015-04-23 00:58:59 +0800

    attempt tree structure

--- tree/tree.js
+++ tree/tree.js
@@ -10,15 +10,36 @@ Your tree should have methods named "addChild" and "contains".
 
 var treeMaker = function(value){
   //tree code goes here!
+  var newTree={};
+  newTree.value= value;
+  newTree.children=[];
+  newTree.addChild=treeMaker.methods.addChild;
+  newTree.caontains=treeMaker.methods.contains;
+
+
+  return newTree;
 };
 
 //methods go here!
 treeMaker.methods = {};
 
-treeMaker.methods.addChild = function(
-){
+treeMaker.methods.addChild = function(value){
+  newTree.children.push(value);
+
+
 };
 
-treeMaker.methods.contains = function(
-){
+treeMaker.methods.contains = function(value){
+  for (var i = 0; i < newTree.children.length; i++) {
+    if (newTree.children[i].value === value) {
+      return true;
+    }
+  }
+  return false;
 };
+
+
+var tree = treeMaker(0);
+tree.addChild(1);
+tree.addChild(2);
+tree.contains(2);   // yields 'true'
