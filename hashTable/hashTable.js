22565eeb8653f756b3185ff7409dc8b0346bef99
raspy8766 <raspy8766@gmail.com>
2015-04-23 10:02:10 -0700

    hashTable is completed but not tested

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -3,23 +3,33 @@
  * The hashtable does not need to resize but it should still handle collisions.
  */
 
-var makeHashTable = function(){
+var makeHashTable = function() {
   var result = {};
-  var storage = [];
-  var storageLimit = 1000;
-  result.insert = function(/*...*/ 
-){
-    // TODO: implement `insert()`
+  result.storage = [];
+  result.storageLimit = 1000;
+
+  result.insert = function(string, value) {
+    var index = getIndexBelowMaxForKey(string, this.storageLimit);
+    if ( Array.isArray(this.storage[index])) {
+      this.storage[index].push({ string: value });
+    } else {
+      this.storage[index] = { string: value };
+    }
   };
 
-  result.retrieve = function(/*...*/ 
-){
-    // TODO: implement `retrieve()`
+  result.retrieve = function(string) {
+    var index = getIndexBelowMaxForKey[string, this.storageLimit];
+    if ( Array.isArray(this.storage[index])) {
+      return this.storage[index][string];
+    } else {
+      return this.storage[index];
+    }
+
   };
 
-  result.remove = function(/*...*/ 
-){
-    // TODO: implement `remove()`
+  result.remove = function(string, value) {
+    var index = getIndexBelowMaxForKey[string, this.storageLimit];
+    delete this.storage[index][string];
   };
 
   return result;
@@ -28,7 +38,7 @@ var makeHashTable = function(){
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
-var getIndexBelowMaxForKey = function(str, max){
+var getIndexBelowMaxForKey = function(str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = (hash<<5) + hash + str.charCodeAt(i);
