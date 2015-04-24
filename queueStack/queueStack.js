2e9f25d03fb9ee61caa3152f1b332d81d97c85c4
Bryan Lee <pygmize.me@gmail.com>
2015-04-24 09:52:08 -0700

    Solution to queueStack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,28 @@
   * Stack Class
   */
 var Stack = function() {
+  var memory = {};
+  var last = 0;
 
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(val){
+    memory[last] = val;
+    last++;
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    if (last !== 0) {
+      var result = memory[--last];
+      delete memory[last];
+      return result;
+    }
+    return null;
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return last;
   };
 };
 
@@ -30,17 +41,23 @@ var Queue = function() {
   var outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
-    // TODO: implement `enqueue`
+  this.enqueue = function(val){
+    inbox.push(val);
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
-    // TODO: implement `dequeue`
+    //the outbox is filled every time it is empty
+    if (!outbox.size()) {
+      while (inbox.size()) {
+        outbox.push(inbox.pop());
+      }
+    }
+    return outbox.pop();
   };
 
   // should return the number of items in the queue
   this.size = function(){
-    // TODO: implement `size`
+    return inbox.size() + outbox.size();
   };
 };
