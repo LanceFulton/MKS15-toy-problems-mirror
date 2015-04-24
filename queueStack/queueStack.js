1ff917284d7d4986acb400a4f2a778dcd5fe8536
sperrow <mattjsperry@gmail.com>
2015-04-24 09:59:30 -0700

    Partial solution for queueStack.js

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,29 @@
   * Stack Class
   */
 var Stack = function() {
+  this.storage = [];
+  this.stackSize = 0;
 
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    // check there is at least one item in stack
+    if( this.storage.length > 0 ) {
+      this.stackSize--;
+      var result = this.storage[this.stackSize];
+      this.storage.pop();
+      return result;
+    }
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this.stackSize;
   };
 };
 
@@ -26,17 +38,21 @@ var Stack = function() {
   */
 var Queue = function() {
   // Use two `stack` instances to implement your `queue` Class
-  var inbox = new Stack();
-  var outbox = new Stack();
+  this.inbox = new Stack();
+  this.outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
+  this.enqueue = function(value){
     // TODO: implement `enqueue`
+    this.inbox.push(value);
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
     // TODO: implement `dequeue`
+    var result = this.inbox[this.outbox.size()];
+    this.outbox.push(1);
+    return result;
   };
 
   // should return the number of items in the queue
