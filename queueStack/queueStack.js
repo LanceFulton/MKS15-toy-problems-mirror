ac31571ddeced01328742eae72da500ccd16bb5d
LanceFulton <lsfaccounts@gmail.com>
2015-04-24 10:08:18 -0700

    queueStack complete.

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -8,16 +8,21 @@
   */
 var Stack = function() {
 
+  this.storage = [];
+
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(input){
+    this.storage.push(input);
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    return this.storage.pop();
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this.storage.length;
   };
 };
 
@@ -26,21 +31,29 @@ var Stack = function() {
   */
 var Queue = function() {
   // Use two `stack` instances to implement your `queue` Class
-  var inbox = new Stack();
-  var outbox = new Stack();
+  this.inbox = new Stack();
+  this.outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
-    // TODO: implement `enqueue`
+  this.enqueue = function(input){
+    this.inbox.storage.push(input);
+    for (var i = this.outbox.storage.length ; i > 0 ; i--){
+      this.outbox.storage[i] = this.outbox.storage[i-1];
+    }
+    this.outbox.storage[0] = input;
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
-    // TODO: implement `dequeue`
+    for (var i = 0 ; i < this.inbox.storage.length-1 ; i++){
+      this.inbox.storage[i] = this.inbox.storage[i+1];
+      this.inbox.storage.pop();
+    }
+    return this.outbox.storage.pop();
   };
 
   // should return the number of items in the queue
   this.size = function(){
-    // TODO: implement `size`
+    return this.inbox.storage.length;
   };
 };
