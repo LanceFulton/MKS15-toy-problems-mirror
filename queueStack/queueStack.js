2712c8cce707ed0d11b9a63ca3043dcb3c46ffda
ivebencrazy <ben@blanketwarriors.com>
2015-04-24 09:59:28 -0700

    [incomplete] Almost complete squeueStack  - Stack is complete  - Queue is close

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -7,17 +7,27 @@
   * Stack Class
   */
 var Stack = function() {
+  this.storage = {};
+  this.count = 0;
 
   // add an item to the top of the stack
-  this.push = function(){
+  this.push = function(value){
+    this.storage[this.count] = value;
+    this.count++;
+    return value;
   };
 
   // remove an item from the top of the stack
   this.pop = function(){
+    this.count--;
+    var valueDestructified = this.storage[this.count];
+    delete this.storage[this.count];
+    return valueDestructified;
   };
 
   // return the number of items in the stack
   this.size = function(){
+    return this.count;
   };
 };
 
@@ -30,17 +40,47 @@ var Queue = function() {
   var outbox = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
-    // TODO: implement `enqueue`
+  this.enqueue = function(value){
+    inbox.push(value);
+    return value;
   };
 
   // called to remove an item from the `queue`
   this.dequeue = function(){
-    // TODO: implement `dequeue`
+    if(outbox.size() === 0){
+      for(var i = 0; i < inbox.size(); i++){
+        outbox.push(inbox.pop());
+      }
+    }
+    return outbox.pop();
   };
 
   // should return the number of items in the queue
   this.size = function(){
-    // TODO: implement `size`
+    return inbox.size() + outbox.size();
   };
 };
+
+// var stack = new Stack();
+// console.log(stack.push(5));
+// console.log(stack.push(12));
+// console.log(stack.push(13));
+// console.log("size: " + stack.size());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log(stack.pop());
+// console.log("size: " + stack.size());
+
+var queue = new Queue();
+console.log(queue.enqueue(5));
+console.log(queue.enqueue(12));
+console.log(queue.enqueue(13));
+console.log(queue.enqueue(15));
+console.log(queue.enqueue(20));
+console.log("size: " + queue.size());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log("size: " + queue.size());
