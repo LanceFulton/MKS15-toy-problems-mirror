de76bf158bfa4713fb6d635d8c786ebc1b3d6669
Trevor Caverly <trevorcaverly@gmail.com>
2015-04-25 09:48:33 -0700

    Working solution to robotPaths

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -23,11 +23,44 @@ var makeBoard = function(n) {
     this[i][j] = !this[i][j];
   }
   board.hasBeenVisited = function(i, j) {
-    return !!this[i][j];
+    return this[i] === undefined || this[j] === undefined || !!this[i][j] ;
   }
   return board;
 };
 
+
+
 var robotPaths = function(n, board, i, j) {
+  if (i===n-1 && j===n-1) {
+    return 1;
+  } else {
+    var upCount = 0;
+    var downCount = 0;
+    var leftCount = 0;
+    var rightCount = 0;
+
+    if (!board.hasBeenVisited(i-1, j)) {
+      board.togglePiece(i,j);
+      upCount = robotPaths(n, board, i-1, j);
+      board.togglePiece(i,j);   
+    }
+    if (!board.hasBeenVisited(i+1, j)) {
+      board.togglePiece(i,j);
+      downCount = robotPaths(n, board, i+1, j);
+      board.togglePiece(i,j);   
+    }
+    if (!board.hasBeenVisited(i, j-1)) {
+      board.togglePiece(i,j);
+      leftCount = robotPaths(n, board, i, j-1);
+      board.togglePiece(i,j);   
+    }
+    if (!board.hasBeenVisited(i, j+1)) {
+      board.togglePiece(i,j);
+      rightCount = robotPaths(n, board, i, j+1);
+      board.togglePiece(i,j);   
+    }
+    return upCount + downCount + leftCount + rightCount;
+  }
+
 }
 
