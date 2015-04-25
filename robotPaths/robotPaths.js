e08f99cc74227d445f2da8754f77e23b403d1f8b
Paul Brady <pablo.brady@gmail.com>
2015-04-25 09:58:32 -0700

    [Significant progress] robotPaths

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -9,7 +9,16 @@
  *
  */
 
+/*
+x x 0 0 0
+0 x x 0 0
+0 0 x 0 0
+0 0 x x x
+0 0 0 0 x
+ */
+
 // A Board class will be useful
+var maxN = 5;
 
 var makeBoard = function(n) {
   var board = [];
@@ -29,5 +38,34 @@ var makeBoard = function(n) {
 };
 
 var robotPaths = function(n, board, i, j) {
-}
+  var depth = 0;
+
+  var recurse = function(aBoard,i,j) {
+    var indexI = i;
+    var indexJ = j;
+    // indexJ++;
+    if(i>=n) { return aBoard; }
+    if(j>=n) { return aBoard; }
+    for(var indexI=i;indexI<maxN;indexI++) {
+      aBoard.togglePiece(indexI, indexJ);
+      recurse(aBoard,indexI,j);
+    }
+    return board;
+  };
+
+  return recurse(n, board, i, j);
+};
+
+var myBoard = makeBoard(5);
+console.log( "PATHS : ", robotPaths(maxN, myBoard, 0, 0) );
+
+
+
+// Psuedocode
+// Begin at 0,0; 
+// Recurse to every element on row one;  (X0000 thru XXXXX)
+//   At every element on row 0, recurse via every element on row 1 (starting at prev col.)
+//   xxx00
+//   00x00
+//   0xx00
 
