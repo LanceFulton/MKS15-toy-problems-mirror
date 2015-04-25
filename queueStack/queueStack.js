ef86c1f5d6457a7bcc113d67ef706e0653a3a1d7
David Tan <davidtansf@gmail.com>
2015-04-25 02:28:21 -0700

    added solution for queueStack

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -46,12 +46,24 @@ var Queue = function() {
   // called to add an item to the `queue`
 Queue.prototype.enqueue = function(value) {
   inbox.push(value);
+  return value + " added";
 };
 
   // called to remove an item from the `queue`
 Queue.prototype.dequeue = function() {
-  outbox.push(inbox.)
-  //need to either modify stack.pop, or have stack methods bounce back and forth, like a slinky?
+  while(inbox._size > 1) {
+    var value = inbox.pop();
+    outbox.push(value);
+  }
+
+  var result = inbox.pop();
+
+  while(outbox._size > 0) {
+    value = outbox.pop();
+    inbox.push(value);
+  }
+
+  return result;
 };
 
   // should return the number of items in the queue
@@ -59,3 +71,16 @@ Queue.prototype.size = function(){
     return inbox.size();
   };
 };
+
+var myQueue = new Queue();
+
+console.log(myQueue.enqueue("A"));
+console.log(myQueue.enqueue("B"));
+console.log(myQueue.enqueue("C"));
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.size());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.size());
+
