e5a080de5e78cb9f15203ad8731dbd7dc9fe5d0f
sperrow <mattjsperry@gmail.com>
2015-04-25 10:00:04 -0700

    Attempted solution for robotPaths.js

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -1,10 +1,10 @@
 /**
- *  
- *  A robot located at the top left corner of a 5x5 grid is trying to reach the 
- *  bottom right corner. The robot can move either up, down, left, or right, 
- *  but cannot visit the same spot twice. How many possible unique paths are 
- *  there to the bottom right corner? 
- *  
+ *
+ *  A robot located at the top left corner of a 5x5 grid is trying to reach the
+ *  bottom right corner. The robot can move either up, down, left, or right,
+ *  but cannot visit the same spot twice. How many possible unique paths are
+ *  there to the bottom right corner?
+ *
  *  make your solution work for a grid of any size.
  *
  */
@@ -29,5 +29,18 @@ var makeBoard = function(n) {
 };
 
 var robotPaths = function(n, board, i, j) {
-}
+  var checkMoves = function(iCurrent, jCurrent) {
+    var moves = {};
+    board[iCurrent-1][jCurrent] === false ? moves.up = true : moves.up = false;
+    board[iCurrent][jCurrent+1] === false ? moves.right = true : moves.right = false;
+    board[iCurrent+1][jCurrent] === false ? moves.down = true : moves.down = false;
+    board[iCurrent][jCurrent-1] === false ? moves.left = true : moves.left = false;
+    return moves;
+  }
 
+  var move = function() {
+
+  }
+};
+
+check moves u,r,d,l
