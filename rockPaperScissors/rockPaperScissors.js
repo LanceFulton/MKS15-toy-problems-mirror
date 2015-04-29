96e894ff8b9c528d7e2d9d4c5881dc78ecf4a917
LanceFulton <lsfaccounts@gmail.com>
2015-04-29 09:21:34 -0700

    Update rock paper scissors.

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,55 +17,25 @@
 *
 */
 
+
 var rockPaperScissors = function () {
-	// create an array of possible inputs for each stage of the sequence
-	var possibles = ['rock', 'paper', 'scissors'];
+    var result = [];
+    var possibilities = ['rock', 'paper', 'scissors'];
 
-	// and a variable to keep track of which answer should currently be taken from the array, for each stage
-	var firstCurrent = 0;
-	var secondCurrent = 0;
-	var thirdCurrent = 0;
+    var recurse = function(roundsLeft, currentRound) {
+      if( roundsLeft === 0 ){
+        result.push(currentRound);
+        return;
+      }
+      for( var i = 0; i < possibilities.length; i++ ){
+        var currentPlay = possibilities[i];
+        recurse( roundsLeft-1, currentRound.concat(currentPlay) );
+      }
+    };
 
-	// and a variable to count how many times the second and third arrays have been run through
-	var secondCount = 0;
-	var thirdCount = 0;
+    recurse(3, []);
 
-	// and result arrays
-	var currentResult = [];
-	var finalResult = [];
-
-  // loop until all possibilities (27) have been realized
-	while(thirdCount <= 27){
-    // push one answer from each stage (first, second, third)
-		currentResult.push(possibles[firstCurrent]);
-		currentResult.push(possibles[secondCurrent]);
-		currentResult.push(possibles[thirdCurrent]);
-		thirdCount++
-		thirdCurrent++
-
-    // push current result to final result and reset current result
-		finalResult.push(currentResult);
-		currentResult = [];
-
-    //after third stage has pushed everything once, restart third stage and increment second stage
-    if (thirdCount % 3 === 0 && thirdCount !== 0){
-			secondCount++;
-			secondCurrent++;
-			thirdCurrent = 0;
-		}
-
-		//after second stage has pushed everything once, restart second stage and increment third stage
-		if (secondCount % 3 === 0 && secondCount !== 0){
-			firstCurrent++;
-			secondCurrent = 0;
-      secondCount = 0;
-		}
-	}
-  	
-  	return finalResult;
-
+    return result;
 };
 
 rockPaperScissors();
-
-
