aa57e8f1a21e14fdd1b038dc1d19434b3b3fa5c9
LanceFulton <lsfaccounts@gmail.com>
2015-05-06 09:35:56 -0700

    Update character frequency.

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -37,5 +37,64 @@
 
 
 var characterFrequency = function(string) {
+
+  // create subarrays
+
+  var storage = {};
+  var result = [];
+
+  var countEm = function(string){
+    for (var i = 0 ; i < string.length ; i++){
+      var character = string[i];
+      if ( storage[character] ){
+        storage[character]++;
+      } else {
+        storage[character] = 1;
+      }
+    }
+  };
+
+  var arrayify = function(object){
+    for (var key in object){
+      var subArray = [];
+      subArray.push(key);
+      subArray.push( object[key] );
+      result.push(subArray);
+    }
+  };
+
+  // sort subarrays by count
+  var sortByQuanitity = function(array){
+    array.sort(function(a,b){
+      return a[1] - b[1];
+    })
+    return array;
+  };
+
+  countEm(string);
+  arrayify(storage);
+  sortByQuantity(result);
+
+
+  // sort equal-count subarrays by character
+
+
   return result;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
